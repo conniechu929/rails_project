5dec194d78f857612707572fa044b53bce4e0edb
Victor Park <09pve09@gmail.com>
2016-12-19 15:46:49 -0800

    second commit

---
+++ config/initializers/yelp.rb
@@ -0,0 +1,6 @@
+Yelp.client.configure do |config|
+  config.consumer_key = 'GrN2-jcpbLc4N6N09hhY9g'
+  config.consumer_secret = 'h_f0iZYX0OZlbu5ZmU_fPKE7m2c'
+  config.token = 'Albn9mhhH8Je-Fs24w_DNW0Q_kWsSa42'
+  config.token_secret = '0cSYCoDLW8ynIKwGN9g2vsuKlis'
+end
